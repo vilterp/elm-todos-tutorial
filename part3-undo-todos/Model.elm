d0a68c918f022d36ae98fa5abad485202bf8c265
Pete Vilter <peter.vilter@socrata.com>
2015-11-10 00:40:34 -0800

    part 3: todos with undo

---
+++ part3-undo-todos/Model.elm
@@ -0,0 +1,124 @@
+module Model where
+
+import Debug
+
+type alias Model =
+  { todos : UndoTodoList
+  , currentText : String
+  , latestId : TodoId
+  }
+
+
+type alias UndoTodoList =
+  { pastStates : List (List Todo) -- most recent first
+  , currentState : List Todo
+  , futureStates : List (List Todo)
+  }
+
+
+emptyUndoTodoList : UndoTodoList
+emptyUndoTodoList =
+  { pastStates = []
+  , currentState = []
+  , futureStates = [] }
+
+
+type alias Todo =
+  { id : TodoId
+  , text : String
+  , done : Bool
+  }
+
+
+type alias TodoId =
+  Int
+
+
+initialModel : Model
+initialModel =
+  { currentText = ""
+  , todos = emptyUndoTodoList
+  , latestId = 0
+  }
+
+
+{-| Has no effect if the given todo id is not found -}
+markDone : TodoId -> Bool -> List Todo -> List Todo
+markDone id done todos =
+  let
+    update todo =
+      if todo.id == id then
+        { todo | done <- done }
+      else
+        todo
+  in
+    List.map update todos
+
+
+delete : TodoId -> List Todo -> List Todo
+delete id todos =
+  List.filter (\todo -> todo.id /= id) todos
+
+
+add : Todo -> List Todo -> List Todo
+add todo todoList =
+  todo :: todoList
+
+-- undo
+
+updateList : (List Todo -> List Todo) -> UndoTodoList -> UndoTodoList
+updateList updateFun undoTodoList =
+  let
+    newList =
+      updateFun undoTodoList.currentState
+  in
+    { pastStates = undoTodoList.currentState :: undoTodoList.pastStates
+    , currentState = newList
+    , futureStates = []
+    }
+
+
+undo : UndoTodoList -> Maybe UndoTodoList
+undo undoTodoList =
+  case undoTodoList.pastStates of
+    mostRecentState :: furtherBackStates ->
+      { pastStates = furtherBackStates
+      , currentState = mostRecentState
+      , futureStates =  undoTodoList.currentState :: undoTodoList.futureStates
+      } |> Just
+
+    _ ->
+      Nothing
+
+
+redo : UndoTodoList -> Maybe UndoTodoList
+redo undoTodoList =
+  case undoTodoList.futureStates of
+    nextStateForward :: furtherForwardStates ->
+      { pastStates = undoTodoList.currentState :: undoTodoList.pastStates
+      , currentState = nextStateForward
+      , futureStates = furtherForwardStates
+      } |> Just
+
+    _ ->
+      Nothing
+
+
+canUndo : UndoTodoList -> Bool
+canUndo undoTodoList =
+  not (List.isEmpty undoTodoList.pastStates)
+
+
+canRedo : UndoTodoList -> Bool
+canRedo undoTodoList =
+  not (List.isEmpty undoTodoList.futureStates)
+
+
+getOrCrash : String -> Maybe a -> a
+getOrCrash msg maybe =
+  case maybe of
+    Just val ->
+      val
+
+    Nothing ->
+      Debug.crash msg
---
+++ part3-undo-todos/Todos.elm
@@ -0,0 +1,137 @@
+module Todos where
+
+import Signal exposing (Signal)
+
+import Html exposing (..)
+import Html.Attributes exposing (..)
+import Html.Events exposing (..)
+import Html.Events.Extra exposing (..)
+import StartApp.Simple as StartApp
+
+import Model exposing (..)
+
+-- controller
+
+type Action
+  = UpdateText String
+  | AddTodo
+  | MarkDone TodoId Bool
+  | Delete TodoId
+  | Undo
+  | Redo
+  | NoOp
+
+
+update : Action -> Model -> Model
+update action model =
+  case action of
+    AddTodo ->
+      let
+        newTodo =
+          { text = model.currentText
+          , id = model.latestId
+          , done = False
+          }
+      in
+        { model
+            | todos <- updateList (\todos -> newTodo :: todos) model.todos
+            , currentText <- ""
+            , latestId <- model.latestId + 1
+        }
+
+    MarkDone id done ->
+      { model | todos <- updateList (\todos -> markDone id done todos) model.todos }
+
+    Delete id ->
+      { model | todos <- updateList (\todos -> delete id todos) model.todos }
+
+    UpdateText newText ->
+      { model | currentText <- newText }
+
+    Undo ->
+      { model | todos <- undo model.todos |> getOrCrash "couldn't undo" }
+
+    Redo ->
+      { model | todos <- redo model.todos |> getOrCrash "couldn't redo" }
+
+    NoOp ->
+      model
+
+
+-- view
+
+view : Signal.Address Action -> Model -> Html
+view addr model =
+  div
+    []
+    [ input
+        [ type' "text"
+        , placeholder "What needs to be done?"
+        , onInput addr (\text -> UpdateText text)
+        , onKeyUp addr (\keyCode ->
+            if keyCode == 13 then AddTodo else NoOp)
+        , value model.currentText
+        ]
+        []
+    , ul
+        []
+        (List.map (viewTodo addr) model.todos.currentState)
+    , div
+        []
+        [ button
+            [ onClick addr Undo
+            , disabled (not <| canUndo model.todos)
+            ]
+            [ text "Undo" ]
+        , button
+            [ onClick addr Redo
+            , disabled (not <| canRedo model.todos)
+            ]
+            [ text "Redo" ]
+        ]
+    ]
+
+
+viewTodo : Signal.Address Action -> Todo -> Html
+viewTodo addr todo =
+  li
+    []
+    ( [ input
+        [ type' "checkbox"
+        , checked todo.done
+        , on
+            "change"
+            targetChecked
+            (\checked -> Signal.message addr (MarkDone todo.id checked))
+        ]
+        []
+      , span
+          [ style
+              [ ("text-decoration", if todo.done then "line-through" else "none") ]
+          ]
+          [ text todo.text ]
+      ] ++
+      if todo.done then
+        [ text " "
+        , a
+          [ onClick addr (Delete todo.id) ]
+          [ text "delete" ]
+        ]
+      else
+        []
+    )
+
+
+-- wiring
+
+app : StartApp.Config Model Action
+app =
+  { model = initialModel
+  , view = view
+  , update = update
+  }
+
+
+main : Signal Html
+main =
+  StartApp.start app
